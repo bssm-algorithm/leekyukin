53d0d8f45012475df9ab04d91301c377c840edf9
leekyukin <leekujin14@gmail.com>
2022-06-15 22:28:28 +0900

    FIX :: 주사위 세개
    - https://www.acmicpc.net/problem/2480

--- src/com/java/IF/Boj2nd2480.java
+++ src/com/java/IF/Boj2nd2480.java
@@ -8,17 +8,10 @@ public class Boj2nd2480 {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        if (a == b && a == c && b == c) {
-            System.out.println(10000 + a * 1000);
-        } else if (a != b && a != c) {
-            if (b == c) {
-                System.out.println(1000 + b * 100);
-            } else {
-                System.out.println(Math.max(a, Math.max(b, c)) * 100);
-            }
-        } else {
-            System.out.println(1000 + a * 100);
-        }
 
-    }
+        if (a == b && a == c && b == c) System.out.println(10000 + a*1000);
+        else if (a == b || a == c) System.out.println(1000 + a*100);
+        else if (b == c) System.out.println(1000 + b*100);
+        else System.out.println(Math.max(a, Math.max(b, c))*100);
+    }ㅈ
 }

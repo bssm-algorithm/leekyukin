89267b5847dedf5bcc5a05384e9ea53dab1a06d6
leekyukin <leekujin14@gmail.com>
2022-06-15 22:28:58 +0900

    FIX :: 오타

--- src/com/java/IF/Boj2nd2480.java
+++ src/com/java/IF/Boj2nd2480.java
@@ -13,5 +13,5 @@ public class Boj2nd2480 {
         else if (a == b || a == c) System.out.println(1000 + a*100);
         else if (b == c) System.out.println(1000 + b*100);
         else System.out.println(Math.max(a, Math.max(b, c))*100);
-    }ㅈ
+    }
 }

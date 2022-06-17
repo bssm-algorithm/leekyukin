08c1e55e9c8a45bd401c208d9efb45f0887c814b
leekyukin <leekujin14@gmail.com>
2022-06-17 20:41:30 +0900

    SOLVE :: 수 정렬하기 3
    - https://www.acmicpc.net/problem/10989
    - 진짜 스스로 너무 대견한게 카운팅 소트 설명글만 대충보고 코드 짬 🥹

---
+++ src/com/java/day03/BojSecond10989.java
@@ -0,0 +1,31 @@
+package com.java.day03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BojSecond10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(bf.readLine());
+
+        int[] count = new int[10000000];
+        int[] arr = new int[N];
+        int max = 0;
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        for(int n : arr) count[n]++;
+
+        for(int i = 0; i <= max; i++) {
+            for(int j = 0; j < count[i]; j++)
+                sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

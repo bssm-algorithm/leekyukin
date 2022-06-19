3881212def583e0148cc4684f3b6f78a78178e05
leekyukin <leekujin14@gmail.com>
2022-06-19 17:56:55 +0900

    SOLVE :: 단어 정렬
    - https://www.acmicpc.net/problem/1181
    - 단계별로 풀기 정렬

---
+++ src/com/java/day05/Boj1181.java
@@ -0,0 +1,24 @@
+package com.java.day05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Boj1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = bf.readLine();
+
+        Arrays.stream(arr)
+                .sorted()
+                .sorted(Comparator.comparing(String::length))
+                .distinct()
+                .forEach(System.out::println);
+    }
+}

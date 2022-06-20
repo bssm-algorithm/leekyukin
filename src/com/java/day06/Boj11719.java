8ab200f6f6ae5e9742940bfe9341e8c0189c6b38
leekyukin <leekujin14@gmail.com>
2022-06-20 19:48:43 +0900

    SOLVE :: 그대로 출력하기 (11719)
    - https://www.acmicpc.net/problem/11719

---
+++ src/com/java/day06/Boj11719.java
@@ -0,0 +1,18 @@
+package com.java.day06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Boj11719 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            System.out.println(input);
+        }
+        sc.close();
+    }
+}

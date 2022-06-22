ef3d6b2d0a95f847c316c4798a328ba66f9ce167
leekyukin <leekujin14@gmail.com>
2022-06-22 11:02:53 +0900

    TIME OVER :: 듣보잡
    - https://www.acmicpc.net/problem/1764
    - 리스트로 품

---
+++ src/com/java/day08/BojTimeOver1764.java
@@ -0,0 +1,44 @@
+package com.java.day08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BojTimeOver1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String N = bf.readLine();
+
+        StringTokenizer st = new StringTokenizer(N, " ");
+
+        int djN = Integer.parseInt(st.nextToken());
+        int bjN = Integer.parseInt(st.nextToken());
+
+        List<String> dj = new ArrayList<>();
+        List<String> bj = new ArrayList<>();
+
+        for (int i = 0; i < djN; i++) {
+            dj.add(bf.readLine());
+        }
+        for (int i = 0; i < bjN; i++) {
+           bj.add(bf.readLine());
+        }
+
+        int number = 0;
+        for (String d : dj) {
+            for (String b : bj) {
+                if(d.equals(b)) {
+                    number++;
+                    sb.append(d).append("\n");
+                }
+            }
+        }
+
+        System.out.println(number + "\n" + sb);
+    }
+}

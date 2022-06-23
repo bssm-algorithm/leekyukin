e3c1d612d7c360c056fd8447ce9103f788f4286a
leekyukin <leekujin14@gmail.com>
2022-06-23 10:18:08 +0900

    SOLVE :: 좌표 압축 (18870)
    - https://www.acmicpc.net/problem/18870
    - Map 이 개사기임 (이중포문 그딴거 필요없게 됨)

---
+++ src/com/java/day09/Boj18870.java
@@ -0,0 +1,51 @@
+package com.java.day09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Boj18870 {
+        static int inputInt(BufferedReader bf) throws IOException {
+            return Integer.parseInt(bf.readLine());
+        }
+        static int toInt(String str) {
+            return Integer.parseInt(str);
+        }
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+            StringBuilder sb = new StringBuilder();
+
+            int N = inputInt(bf);
+
+            List<Integer> origin = new ArrayList<>();
+
+            String input = bf.readLine();
+            StringTokenizer st = new StringTokenizer(input, " ");
+            for(int i = 0; i < N; i++) {
+                origin.add(toInt(st.nextToken()));
+            }
+
+            int[] sorted = origin.stream()
+                    .sorted()
+                    .distinct()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < sorted.length; i++)
+                map.put(sorted[i], i);
+
+
+            for (int i = 0; i < origin.size(); i++) {
+                if(map.containsKey(origin.get(i))) {
+                    sb.append(map.get(origin.get(i))).append(" ");
+                }
+            }
+
+            System.out.println(sb);
+     }
+}

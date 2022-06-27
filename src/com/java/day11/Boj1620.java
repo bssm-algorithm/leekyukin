1e8fea8f3bacf8ff2903a442ba32c31ded0ab0c7
leekyukin <leekujin14@gmail.com>
2022-06-27 16:51:01 +0900

    SOLVE :: 나는야 포켓몬 마스터 이다솜
    - https://www.acmicpc.net/problem/1620

---
+++ src/com/java/day11/Boj1620.java
@@ -0,0 +1,47 @@
+package com.java.day11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Boj1620 {
+    static int strToInt(String str) {
+        return Integer.parseInt(str);
+    }
+    static boolean isString(String str) {
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int N = strToInt(st.nextToken());
+        int M = strToInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+        String[] arr = new String[N+1];
+        for (int i = 0; i < N; i++) {
+            String mon = bf.readLine();
+            map.put(mon, i + 1);
+            arr[i + 1] = mon;
+        }
+
+        for(int i = 0; i < M; i++) {
+            String mon = bf.readLine();
+            if (isString(mon)) {
+                System.out.println(map.get(mon));
+            }
+            else {
+                int intMon = Integer.parseInt(mon);
+                System.out.println(arr[intMon]);
+            }
+        }
+    }
+}

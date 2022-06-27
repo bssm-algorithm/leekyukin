b37eddb01034a23bad5845d0cd42c111560f7015
leekyukin <leekujin14@gmail.com>
2022-06-27 17:24:34 +0900

    SOLVE :: 숫자 카드 2
    - https://www.acmicpc.net/problem/10816

---
+++ src/com/java/day11/Boj10816.java
@@ -0,0 +1,45 @@
+package com.java.day11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Boj10816 {
+    static int strToInt(String str) {
+        return Integer.parseInt(str);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = strToInt(bf.readLine());
+        List<String> list = new ArrayList<>();
+        StringTokenizer first = new StringTokenizer(bf.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            list.add(first.nextToken());
+        }
+
+        int M = strToInt(bf.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        StringTokenizer second = new StringTokenizer(bf.readLine(), " ");
+        String[] keys = new String[M];
+        for (int i = 0; i < M; i++) {
+            String key = second.nextToken();
+            keys[i] = key;
+            map.put(key, 0);
+        }
+
+        for (int i = 0; i < N; i++) {
+            String key = list.get(i);
+            if(map.containsKey(key)) {
+                map.put(key, map.get(key)+1);
+            }
+        }
+
+        for (String key : keys) {
+            sb.append(map.get(key)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}

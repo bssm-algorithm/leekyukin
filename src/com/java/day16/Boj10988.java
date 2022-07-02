e0c5013b0a1754b3f71d115107a675a96d1d1b5c
leekyukin <leekujin14@gmail.com>
2022-07-02 16:00:26 +0900

    SOLVE :: 팰린드롭인지 확인하기 (10988)
    - https://www.acmicpc.net/problem/10988

---
+++ src/com/java/day16/Boj10988.java
@@ -0,0 +1,34 @@
+package com.java.day16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+
+        int cnt = 0;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) == str.charAt(str.length()-1-i)) {
+                cnt++;
+            }
+        }
+
+        int want;
+        if (str.length() % 2 == 1) {
+            want = (str.length()-1) / 2;
+        }
+        else {
+            want = str.length() / 2;
+        }
+
+        if (want == cnt) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}

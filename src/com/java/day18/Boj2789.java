43a047cd9636454bda1c33707cdd3791f956f8f4
leekyukin <leekujin14@gmail.com>
2022-07-04 21:13:15 +0900

    SOLVE :: 유학 금지
    - https://www.acmicpc.net/problem/2789

---
+++ src/com/java/day18/Boj2789.java
@@ -0,0 +1,32 @@
+package com.java.day18;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2789 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+
+       // CAMBRIDGE
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'C'
+                    || c == 'A'
+                    || c == 'M'
+                    || c == 'B'
+                    || c == 'R'
+                    || c == 'I'
+                    || c == 'D'
+                    || c == 'G'
+                    || c == 'E'
+            ) continue;
+            sb.append(c);
+        }
+
+        System.out.println(sb);
+    }
+}

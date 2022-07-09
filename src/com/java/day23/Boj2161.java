e951bf4ec74703545aa745a2a0bc4c2f74d80f9f
leekyukin <leekujin14@gmail.com>
2022-07-09 19:20:15 +0900

    SOLVE :: 카드 1 (2161)
    - https://www.acmicpc.net/problem/2161

---
+++ src/com/java/day23/Boj2161.java
@@ -0,0 +1,27 @@
+package com.java.day23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Boj2161 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> cards = new ArrayList<>();
+        for (int i = 0 ; i < N; i++) {
+            cards.add(i+1);
+        }
+
+        int n = 0;
+        while (cards.size() != 1) {
+            System.out.print(cards.remove(0) + " ");
+            cards.add(cards.size()-1, cards.remove(0));
+            if (cards.size() == 1) System.out.println(cards.get(0));
+        }
+    }
+}
